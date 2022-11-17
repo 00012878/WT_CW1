225b1ca84d3b659ca72b51657be4f2c607670293
00012878 <115094738+00012878@users.noreply.github.com>
2022-11-17 11:04:51 +0500

    Added theme switch function
    Persistent theme switch functionality implemented

--- js/master.js
+++ js/master.js
@@ -1,6 +1,7 @@
 const header = document.querySelector("header")
 const headerClass = document.querySelector(".header")
 const headerTop = document.querySelector(".header-top")
+const themeSwitcher = document.querySelector("#theme-switcher")
 
 // Using Intersection Observer Object to change height and backdrop-filter when header becomes sticky.
 const observer = new IntersectionObserver((entries) => {
@@ -32,3 +33,38 @@ function animateY(element) {
     element.style.transform = "translateY(0)"
     element.style.opacity = "1"
 }
+
+// Theme switching logic.
+try {
+    // Checking if theme value is stored in the local storage.
+    if (window.localStorage.getItem("theme") === "dark") {
+        document.body.classList.replace("light-theme", "dark-theme")
+        themeSwitcher.textContent = "Light Mode"
+        themeSwitcher.style.backgroundColor = "#e6e6e6"
+        themeSwitcher.style.color = "black"
+    } else if (window.localStorage.getItem("theme") === "light") {
+        document.body.classList.replace("dark-theme", "light-theme")
+        themeSwitcher.textContent = "Dark Mode"
+        themeSwitcher.style.backgroundColor = "#242424"
+        themeSwitcher.style.color = "#ebebeb"
+    }
+} catch (error)  {
+    console.log("Could not retrieve localStorage item \"theme\"")
+}
+
+// Change theme on theme button click.
+themeSwitcher.addEventListener("click", () => {
+    if (document.body.className === "light-theme") {
+        document.body.classList.replace("light-theme", "dark-theme")
+        themeSwitcher.textContent = "Light Mode"
+        themeSwitcher.style.backgroundColor = "#e6e6e6"
+        themeSwitcher.style.color = "black"
+        window.localStorage.setItem("theme", "dark")
+    } else if (document.body.className === "dark-theme") {
+        document.body.classList.replace("dark-theme", "light-theme")
+        themeSwitcher.textContent = "Dark Mode"
+        themeSwitcher.style.backgroundColor = "#242424"
+        themeSwitcher.style.color = "#ebebeb"
+        window.localStorage.setItem("theme", "light")
+    }
+})
